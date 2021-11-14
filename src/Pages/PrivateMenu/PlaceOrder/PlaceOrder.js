e0a70e4f98cd66cf4ef3d424c02706f36e938a25
Mohosin Islam Palash <mohosinislampolash@gmail.com>
2021-11-15 00:13:20 +0600

    Place Order UI Done

--- src/Pages/PrivateMenu/PlaceOrder/PlaceOrder.js
+++ src/Pages/PrivateMenu/PlaceOrder/PlaceOrder.js
@@ -4,6 +4,13 @@ import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 import useAuth from '../../../contexts/useAuth';
+import './PlaceOrder.css'
+
+import b1 from '../../../images/demo/bbb/b1.png'
+import a1 from '../../../images/demo/aaa/a1.png'
+
+import b2 from '../../../images/demo/bbb/b1.png'
+import b3 from '../../../images/demo/bbb/b1.png'
 
 const PlaceOrder = () => {
     const { user, logout } = useAuth();
@@ -27,23 +34,40 @@ const PlaceOrder = () => {
                 if (res.data.insertedId) {
                     alert('Your Order is placed Successfully!');
                     reset();
+                    document.getElementById('confirm-msg').style.display = "block";
                 }
             })
     };
 
     return (
-        <div className="add-service">
-            <h1>Place Order</h1>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input type="text" defaultValue={packages.title} {...register("title", { required: true, maxLength: 50 })} />
-                <input type="email" defaultValue={user.email} {...register("email", { required: true, maxLength: 100 })} />
-                <input type="text" defaultValue={user.displayName} {...register("name", { required: true, maxLength: 100 })} />
-                <input type="number" defaultValue={packages.fare}{...register("fare", { required: true })} placeholder="Fare" />
-                <input type="text" {...register("address", { required: true, maxLength: 150 })} placeholder="Address" />
-                <input type="number" {...register("contact", { required: true })} placeholder="Contact No" />
-                <input type="submit" value="PLACE ORDER" />
-            </form>
-        </div>
+        <>
+            <img src={a1} className="banner" alt="" />
+            <div className="banner-grid">
+                <div>
+                    <div className="place-order">
+
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <input type="text" defaultValue={packages.title} {...register("title", { required: true, maxLength: 50 })} />
+                            <input type="email" defaultValue={user.email} {...register("email", { required: true, maxLength: 100 })} />
+                            <input type="text" defaultValue={user.displayName} {...register("name", { required: true, maxLength: 100 })} />
+                            <input type="number" defaultValue={packages.fare}{...register("fare", { required: true })} placeholder="Fare" />
+                            <input type="text" {...register("address", { required: true, maxLength: 150 })} placeholder="Address" />
+                            <input type="number" {...register("contact", { required: true })} placeholder="Contact No" />
+                            <input type="submit" className="submit-button" value="PLACE ORDER" />
+                            <p id="confirm-msg" style={{ color: 'green', display: 'none' }}>Your Order is placed successfully!
+                                <Link to={'/home'}><strong>Go to home</strong></Link>
+                            </p>
+                        </form>
+
+                    </div>
+                </div>
+                <div className="login-info">
+                    <img src={b1} />
+
+                </div>
+            </div>
+        </>
+
     );
 };
 
