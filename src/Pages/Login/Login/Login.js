240e8122c0f7e20d53457f19c4870242675797b0
Mohosin Islam Palash <mohosinislampolash@gmail.com>
2021-11-14 07:02:17 +0600

    Login UI Done

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import useAuth from '../../../contexts/useAuth';
+import background from '../../../images/basic/login_bg.jpg';
+import logo from '../../../images/basic/logo.png';
+import './Login.css'
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendEmailVerification, updateProfile } from "firebase/auth";
 const Login = () => {
     const { googleSignIn } = useAuth();
@@ -22,9 +25,20 @@ const Login = () => {
     }
 
     return (
-        <div>
-            <h1>This is Login</h1>
-            <button onClick={handleGoogleLogin} className="btn btn-warning">Login with GOOGLE</button>
+        <div style={{ backgroundImage: `url(${background})`, backgroundSize: 'cover', height: '650px', marginLeft: 'auto' }}>
+
+            <div className="banner-grid">
+                <div>
+
+                </div>
+                <div className="login-info">
+                    <img src={logo} style={{ height: '250px', width: '250px' }} />
+                    <h5 style={{ padding: '0px 20px' }}>Warning: To get our services, you must login with us. <br /> No complexity, No late! <br /> Just tap on the below button and<br />  Sign in with GOOGLE</h5>
+                    <button onClick={handleGoogleLogin} className="btn btn-danger"><strong>Sign in with GOOGLE</strong></button>
+                </div>
+            </div>
+
+
         </div>
     );
 };
